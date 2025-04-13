4475f4c6d7776379c5e8ce8f3768031db27e570b
Funkicide <knel@skbkontur.ru>
2025-04-13 10:59:35 +0300

    feat: save my custom config

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,5 @@
+return {
+  "tpope/vim-fugitive",
+  cmd = { "G", "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete" },
+  dependencies = { "tpope/vim-rhubarb" }, -- Optional: GitHub integration
+}
